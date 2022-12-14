2f5439f131810fc45402eebc6c457497356bc085
thanhthanhagl <diepthanhthanh.agl@gmail.com>
2022-12-14 11:59:59 +0700

    update 8%

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("main.js");
+$(document).ready(function () {
+    $(".c-header__item").click(function(){
+        $(".c-header__submenu").removeClass("is-click");
+        var submenu = $(this).next();
+        if(submenu){
+            $(this).next().addClass("is-click");
+        }  
+        $(this).click(function(){
+            $(this).next().toggleClass("is-click");
+        });     
+    });
+});
