f2633c3139318e91871f7fb1eb810af2c6a46540
thanhthanhagl <diepthanhthanh.agl@gmail.com>
2022-12-15 17:29:08 +0700

    15%

--- src/_public/assets/js/main.js
+++ src/_public/assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    //-------------Menu click
     $(".c-header__item").click(function(){
         $(".c-header__submenu").removeClass("is-click");
         var submenu = $(this).next();
@@ -9,4 +10,17 @@ $(document).ready(function () {
             $(this).next().toggleClass("is-click");
         });     
     });
+    //----------slick slider------------
+  $('.c-mainvisual__slider').slick({
+    infinite: true,
+    fade: true,
+    autoplay: true,
+    autoplayspeed: 3000,
+    speed: 2000,
+    arrows: false,
+    dots: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    cssEase:'linear'
+  });
 });
